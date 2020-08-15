db543660f3d88eaa9184e16a70e4a04aefc154e9
sabrina mahjabin <mahjabin.sabrina@yahoo.com>
2020-08-15 13:16:44 -0400

    added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+class HtmlDocument:
+    def __init__(self):
+        pass
+
+class HtmlManager:
+    def __init__(self):
+        pass
+
+class AWSManager:
+    def __init__(self):
+        pass
+
+    
