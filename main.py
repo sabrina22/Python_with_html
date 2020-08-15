541668aa3ff908a340a4bc2e8540f0245409e551
sabrina mahjabin <mahjabin.sabrina@yahoo.com>
2020-08-15 14:37:25 -0400

    removed practice file

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ class HtmlDocument:
 class HtmlManager:
     def __init__(self):
         pass
+    def creating_html(self):
+        file = open('demo.html', 'wb')
+        
+        content = """<html>
+        <head></head>
+        <body><p>Hello World!</p></body>
+        </html>"""
+
+        file.write(content)
+        file.close()
+    
+
 
 class AWSManager:
     def __init__(self):
